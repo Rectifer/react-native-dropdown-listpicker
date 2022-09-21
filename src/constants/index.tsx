f23aa307dd6ef87dedbbdeb02273603572f8ff60
Kishore Kumar Sharma <kishoresharma914@gmail.com>
2022-09-21 14:22:20 +0530

    Update index.tsx

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -3,7 +3,7 @@ import {Dimensions} from 'react-native';
 export const WIDTH = Dimensions.get('screen').width;
 export const HEIGHT = Dimensions.get('screen').height;
 export const ROOT_WIDTH = '90%';
-export const ITEM_ROW_HEOGHT = 40;
+export const ITEM_ROW_HEOGHT = 50;
 export const DM_HEIGHT = 50;
 export const SB_HEIGHT = 45;
 export const PLACEHOLDER = 'select item(s)';
